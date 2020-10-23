a1ae895b58ae20360b6d1586b9235b50de49f3c6
Gary <garyberger@forwardnetworks.com>
2020-10-23 09:27:02 -0400

    Add versioned api
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- src/clj_forward/api.clj
+++ src/clj_forward/api.clj
@@ -1,17 +1,25 @@
 (ns clj-forward.api
-  (:require [martian.core :as martian]))
+  (:require [clj-forward.core :refer [bootstrap-openapi add-custom-header]]
+            [cheshire.core :refer [parse-string]]
+            [clojure.java.io :as io]))
 
-(defn create-fwd-context-custom [{:keys [swagger]}]
-  (martian/bootstrap swagger
-                     [{:route-name  :load-pet
-                       :path-parts  ["/pets/" :id]
-                       :method      :get
-                       :path-schema {:id s/Int}}
+;(defn create-fwd-context-custom [{:keys [swagger]}]
+;  (martian/bootstrap swagger
+;                     [{:route-name  :load-pet
+;                       :path-parts  ["/pets/" :id]
+;                       :method      :get
+;                       :path-schema {:id s/Int}}
+;
+;                      {:route-name  :create-pet
+;                       :produces    ["application/xml"]
+;                       :consumes    ["application/xml"]
+;                       :path-parts  ["/pets/"]
+;                       :method      :post
+;                       :body-schema {:pet {:id   s/Int
+;                                           :name s/Str}}}]))
 
-                      {:route-name  :create-pet
-                       :produces    ["application/xml"]
-                       :consumes    ["application/xml"]
-                       :path-parts  ["/pets/"]
-                       :method      :post
-                       :body-schema {:pet {:id   s/Int
-                                           :name s/Str}}}]))
+(defn create-fwd-context [{:keys [server version user password debug insecure]}]
+  (bootstrap-openapi server (parse-string (slurp (io/resource (format "api-%s.json" version))))
+                     (add-custom-header user password debug insecure)))
+
+
